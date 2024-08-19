39bf45634309981514691cef07e068b3721da46a
Bradford Powell <bpow@drpowell.org>
2024-08-19 12:32:08 -0400

    rename some things, separate out barcode from readname-items pkm

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ impl PrimaryKeyMap {
         }
     }
 
-    fn get(&mut self, key: &str) -> u16 {
+    fn key_for_value(&mut self, key: &str) -> u16 {
         match self.map.get(key) {
             Some(value) => *value,
             None => {
@@ -27,24 +27,25 @@ impl PrimaryKeyMap {
         }
     }
 
-    fn get_key(&self, key: u16) -> Option<&String> {
+    fn value_for_key(&self, key: u16) -> Option<&String> {
         self.reverse_map.get(key as usize)
     }
 }
 
-fn name_to_key(name: &str, primary_key_map: &mut PrimaryKeyMap) -> [u16; 4] {
+fn name_to_readname_key(name: &str, primary_key_map: &mut PrimaryKeyMap) -> [u16; 4] {
     let name_parts = name.split(':').collect::<Vec<&str>>();
     let mut key = [0 as u16; 4];
-    key[0] = primary_key_map.get(&name_parts[0..4].join(":"));
+    key[0] = primary_key_map.key_for_value(&name_parts[0..4].join(":"));
     for i in 0..3 {
-        key[i + 1] = primary_key_map.get(name_parts[4+i]);
+        key[i + 1] = primary_key_map.key_for_value(name_parts[4+i]);
     }
     return key;
 }
 
 fn main() {
     env_logger::init();
-    let mut primary_key_map = PrimaryKeyMap::new();
+    let mut readname_part_pkm = PrimaryKeyMap::new();
+    let mut comment_pkm = PrimaryKeyMap::new();
     let mut readnames_to_comments: HashMap<[u16; 4], u16> = HashMap::new();
     let mut args = std::env::args().collect::<Vec<String>>();
 
@@ -55,7 +56,7 @@ fn main() {
             let line = line.unwrap();
             let parts: Vec<&str> = line.splitn(2, ' ').collect();
             let read_name = parts[0];
-            let key = name_to_key(read_name, &mut primary_key_map);
+            let readname_key = name_to_readname_key(read_name, &mut readname_part_pkm);
             let fastq_comment = parts[1];
             let fastq_comment = if fastq_comment.starts_with("1:N:0:") || fastq_comment.starts_with("2:N:0:") {
                 format!("BC:Z:{}", &fastq_comment[6..])
@@ -63,10 +64,11 @@ fn main() {
                 warn!("Comment {} does not start with 1:N:0: or 2:N:0:, will use XC tag", fastq_comment);
                 format!("XC:Z:{}", fastq_comment)
             };
-            readnames_to_comments.insert(key, primary_key_map.get(&fastq_comment));
+            readnames_to_comments.insert(readname_key, comment_pkm.key_for_value(&fastq_comment));
         }
-        info!("Read {} comments after reading from {}", readnames_to_comments.len(), barcodefile);
-        info!("Number of strings 'interned': {}", primary_key_map.reverse_map.len());
+        info!("Read comments for {} readnames after reading from {}", readnames_to_comments.len(), barcodefile);
+        info!("Number of distinct readname parts 'interned': {}", readname_part_pkm.reverse_map.len());
+        info!("Number of distinct comments 'interned': {}", comment_pkm.reverse_map.len());
     }
 
     for line in stdin().lock().lines() {
@@ -75,10 +77,10 @@ fn main() {
             println!("{}", line)
         } else {
             let row = line.split('\t').collect::<Vec<&str>>();
-            let key = name_to_key(&format!("@{}", row[0]), &mut primary_key_map);
-            match readnames_to_comments.get(&key) {
+            let name_key = name_to_readname_key(&format!("@{}", row[0]), &mut readname_part_pkm);
+            match readnames_to_comments.get(&name_key) {
                 Some(comment_key) => {
-                    let comment = primary_key_map.get_key(*comment_key).unwrap();
+                    let comment = comment_pkm.value_for_key(*comment_key).unwrap();
                     if !comment.starts_with("BC:Z:") {
                         warn!("Comment looks wrong: {} for {}", comment, row[0]);
                     }
